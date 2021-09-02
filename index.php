f41a78a927f5f39b1658229094dbe1188ee0fc23
Bhagya Mudgal <76253604+bhagyamudgal@users.noreply.github.com>
2021-09-02 23:36:07 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ require_once "includes/header.php";
 <main role="main" class="container-md home-main">
     <img src="./public/banner-home.jpg" class="banner-home mt-4" alt="Banner-Home">
     <h1 class="mt-5">Presence - Registration Tracking System</h1>
-    <p class="lead mt-3">Presence is an registration tracking system. Users register themselves by filling out registration form and their entries is recorded in database which can be seen by admin.</p>
+    <p class="lead mt-3">Presence is a Registration Tracking System. Users register themselves by filling out the registration form and their entries are recorded in the database which can be seen by the admin.</p>
     <div class="container-md home-btn-group px-0">
         <a href="registrationform.php" type="button" id="home-registration-form-btn" class="btn btn-dark btn-lg mt-3">Registration Form</a>
         <a href="submissions.php" type="button" id="home-submissions-btn" class="btn btn-dark btn-lg mx-sm-3 mt-3">Submissions</a>
